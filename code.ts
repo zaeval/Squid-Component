9ea1033b5074b91ebaabf33364ecfe544377acab
juheesvt <kangjuhee18@kookmin.ac.kr>
2021-10-13 22:30:15 +0900

    localstorage

--- code.ts
+++ code.ts
@@ -7,32 +7,85 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-figma.timer.start(1)
+
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+// figma.ui.onmessage = msg => {
+//   // One way of distinguishing between different types of messages sent from
+//   // your HTML page is to use an object with a "type" property like this.
+//   if (msg.type === 'create-rectangles') {
+//     const nodes: SceneNode[] = [];
+//     for (let i = 0; i < msg.count; i++) {
+//       const rect = figma.createRectangle();
+//       rect.x = i * 150;
+//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//       figma.currentPage.appendChild(rect);
+//       nodes.push(rect);
+//     }
+//     figma.currentPage.selection = nodes;
+//     figma.viewport.scrollAndZoomIntoView(nodes);
+//   }
+//
+//   // Make sure to close the plugin when you're done. Otherwise the plugin will
+//   // keep running, which shows the cancel button at the bottom of the screen.
+//   figma.closePlugin();
+// };
+figma.ui.onmessage = async (msg) => {
+    if (msg.event === 'set-async') {
+        figma.clientStorage.setAsync(msg.key, msg.value);
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+    else if(msg.event === 'get-async' && msg.key !== null){
+        const ret = await figma.clientStorage.getAsync(msg.key);
+        figma.ui.postMessage({
+            event: 'get-async',
+            key: msg.key,
+            value: ret?ret:null
+        })
+    }
+}
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+function getChild() {
 
-//TODO: 해당 컴포넌트가 플러그인이 적용이 되어야하는지 아닌지 여부 저장
-//https://www.figma.com/plugin-docs/api/figma-clientStorage/
+}
 
-//TODO: 해당 컴포넌트의 사이즈를 수정중인지 아닌지를 잡는 이벤트 리스너 정의
+setInterval(() => {
+    const selection = figma.currentPage.selection;
+    if (selection.length == 1) {
+        // figma.ui.postMessage(selection);
+        if (selection[0].type == "FRAME") {
+            figma.ui.postMessage({
+                status: 1,
+                event: 'select-change',
+                type: selection[0].type,
+            });
+        } else if (selection[0].type == "COMPONENT") {
+            figma.ui.postMessage({
+                status: 0,
+                event: 'select-change',
+                type: selection[0].type,
+                children: selection[0].children,
+                id: selection[0].id
+            })
+        } else if (selection[0].type == "INSTANCE") {
+            figma.ui.postMessage({
+                status: 0,
+                event: 'select-change',
+                type: selection[0].type,
+                children: selection[0].children,
+                id: selection[0].id
+            })
+        }
+        /*figma.ui.postMessage({
+            status: 0,
+            type: selection[0].type
+        })*/
+
+    } else {
+        figma.ui.postMessage({
+            status: 2,
+            event: 'select-change',
+            id: null,
+        })
+    }
+}, 500);
